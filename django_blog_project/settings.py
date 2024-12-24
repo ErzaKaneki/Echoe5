5cf9be3668697e97b4706aa540f171d3fbb2e84e
Erza Kaneki <nevin.kadlec@gmail.com>
2024-12-23 19:47:57 -0600

    Updated models and settings to resize profile pictures from AWS S3

--- django_blog_project/settings.py
+++ django_blog_project/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('S_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
--- users/models.py
+++ users/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.core.files.storage import default_storage
 from django.contrib.auth.models import User
+from PIL import Image
+from io import BytesIO
+import boto3
+from django.core.files.base import ContentFile
 
 
     
@@ -14,8 +18,33 @@ class Profile(models.Model):
         )
     
     def save(self, *args, **kwargs):
-        print(self.profile_picture.url)  # This will print the URL of the uploaded image.
-        super(Profile, self).save(*args, **kwargs)
+        if self.profile_picture:
+            # resize image
+            img = Image.open(self.profile_picture)
+            img = img.resize((150, 150), Image.Resampling.LANCZOS)
+            
+            # Save resized image to BytesIO object
+            buffer = BytesIO()
+            img.save(buffer, format='JPEG')
+            buffer.seek(0)
+            
+            # Create a new ContentFile
+            file_content = ContentFile(buffer.getvalue())
+            file_name = f'{self.user.username}_profile.jpg'
+            
+            # Save the resized image to s3
+            s3 = boto3.client('s3')
+            bucket_name = 'echoe5-files'
+            s3.put_object(Bucket=bucket_name, Key=f'profile_pics/{file_name}', Body=file_content)
+            
+            # Update the profile picture field to the point to S3 URL
+            self.profile_picture = f'profile_pics/{file_name}'
+            
+        super().save(*args, **kwargs)
+    
+    # def save(self, *args, **kwargs):
+    #     print(self.profile_picture.url)  # This will print the URL of the uploaded image.
+    #     super(Profile, self).save(*args, **kwargs)
     
     def __str__(self):
         return f'{self.user.username} Profile'
