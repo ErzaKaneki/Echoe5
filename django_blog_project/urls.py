e6f2c2d46b33b386f0796e05623457dea3a37c32
Erza Kaneki <nevin.kadlec@gmail.com>
2025-01-20 23:48:46 -0600

    Implemented api documentation

--- django_blog_project/urls.py
+++ django_blog_project/urls.py
@@ -2,14 +2,29 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 import logging
 
 logger = logging.getLogger(__name__)
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Blog API",
+        default_version='v1',
+        description="API documentation for the blog application",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')), # This will include all those blog-specific URLs
     path('', include('users.urls')),  # This will include all those user-specific URLs
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
+    path('redoc/', schema_view.with_ui('redoc', cache_tiemout=0)),
 ]
 
 if settings.DEBUG:
