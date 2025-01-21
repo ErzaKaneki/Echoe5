bd45747914b5b0e44f0eb38a44013f7fa2078c4b
Erza Kaneki <nevin.kadlec@gmail.com>
2025-01-20 23:37:11 -0600

    Added api urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,6 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .api.v1 import views as api_views
 from .views import (
     PostListView,
     PostDetailView,
@@ -11,6 +13,9 @@ from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 
+router = DefaultRouter()
+router.register(r'posts', api_views.PostViewSet)
+
 urlpatterns = [
     path('', PostListView.as_view(), name = 'blog-home'),
     path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
@@ -19,4 +24,5 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='blog-about'),
+    path('api/v1/', include(router.urls)),
 ]
