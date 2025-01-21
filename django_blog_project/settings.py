2f0a17198c2f3ee9db9004f96fb6c01ca6119f0f
Erza Kaneki <nevin.kadlec@gmail.com>
2025-01-20 23:27:12 -0600

    Updated settings.py for implementation of rest api

--- django_blog_project/settings.py
+++ django_blog_project/settings.py
@@ -40,7 +40,29 @@ INSTALLED_APPS = [
     'crispy_forms',
     'storages',
     'django_extensions',
+    'rest_framework',
+    'rest_framework_simplejwt',
 ]
+
+# Rest Framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.AcceptHeaderVersioning',
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '1000/day',
+    }
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
